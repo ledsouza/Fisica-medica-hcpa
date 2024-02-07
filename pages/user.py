b5eab13f4945d508d87b79d045f4c9a5d963dbf4
Leandro Souza <leandro.souza.159@gmail.com>
2024-02-07 16:28:09 -0300

    Removido st.write para teste

--- pages/user.py
+++ pages/user.py
@@ -2,8 +2,6 @@ import streamlit as st
 from menu import menu_with_redirect
 
 # Redirect to app.py if not logged in, otherwise show the navigation menu
-st.write(st.session_state)
-
 menu_with_redirect()
 
 st.title("This page is available to all users")
