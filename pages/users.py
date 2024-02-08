2c8fe948e29911f6de7d4ec06fd877310b21a3ba
Leandro Souza <leandro.souza.159@gmail.com>
2024-02-08 08:46:05 -0300

    Removido imports desnecessários

--- pages/users.py
+++ pages/users.py
@@ -1,9 +1,4 @@
 import streamlit as st
-import streamlit_authenticator as stauth
-import yaml
-from yaml.loader import SafeLoader
-from users_management import UsersManagement
-import time
 from menu import menu_with_redirect
 
 menu_with_redirect()
