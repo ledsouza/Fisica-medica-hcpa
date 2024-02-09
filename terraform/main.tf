2b41c2692ff05cad8ae40febb4a7670bd37a2d8b
Leandro Souza <leandro.souza.159@gmail.com>
2024-02-09 08:57:49 -0300

    Configurando secrets no terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,37 +9,84 @@ terraform {
 
 provider "google" {
   project = "fisica-medica-hcpa"
+  region  = "us-central1"
 }
 
-resource "google_service_account" "cloudrun_service_identity" {
+resource "google_service_account" "account" {
   account_id = "fisica-medica-hcpa"
 }
 
-resource "google_secret_manager_secret" "default" {
+resource "google_secret_manager_secret" "secret" {
   secret_id = "s3-fisica-medica-hcpa"
+  replication {
+    automatic = true
+  }
 }
 
-resource "google_secret_manager_secret_iam_member" "default" {
-  secret_id = google_secret_manager_secret.default.id
+resource "google_secret_manager_secret" "access_key" {
+  name = "AWS_ACCESS_KEY_ID"
+}
+
+resource "google_secret_manager_secret_version" "access_key" {
+  secret      = google_secret_manager_secret.secret.id
+  secret_data = "AKIAUJAUJNOCJPEMMPIO"
+}
+
+resource "google_secret_manager_secret" "secret_access_key" {
+  name = "AWS_SECRET_ACCESS_KEY"
+}
+
+resource "google_secret_manager_secret_version" "secret_access_key" {
+  secret      = google_secret_manager_secret.secret.id
+  secret_data = "1tptqziFKNWboo3ee8HKtBWOF8P3X9uYXYIy8NvM"
+}
+
+resource "google_secret_manager_secret" "default_region" {
+  name = "AWS_DEFAULT_REGION"
+}
+
+resource "google_secret_manager_secret_version" "default_region" {
+  secret      = google_secret_manager_secret.secret.id
+  secret_data = "us-east-1"
+}
+
+resource "google_secret_manager_secret_iam_member" "access" {
+  secret_id = google_secret_manager_secret.secret.id
   role      = "roles/secretmanager.secretAccessor"
-  # Grant the new deployed service account access to this secret.
-  member     = "serviceAccount:${google_service_account.default.email}"
-  depends_on = [google_secret_manager_secret.default]
+  member     = "serviceAccount:${google_service_account.account.email}"
+  depends_on = [google_secret_manager_secret.secret]
 }
 
-resource "google_cloud_run_v2_service" "env_variable_secret" {
-  name     = "cloudrun-srv-env-var-secret"
+resource "google_cloud_run_v2_service" "default" {
+  name     = "mnmanagement"
   location = "us-central1"
-  ingress  = "INGRESS_TRAFFIC_ALL"
 
   template {
     containers {
-      image = "us-docker.pkg.dev/cloudrun/container/hello"
+      image = "us-central1-docker.pkg.dev/fisica-medica-hcpa/fisica-medica-repo/mnmanagement"
       env {
-        name = "MY_SECRET"
+        name = "AWS_ACCESS_KEY_ID"
         value_source {
           secret_key_ref {
-            secret  = google_secret_manager_secret.default.secret_id
+            secret  = google_secret_manager_secret.access_key.secret_id
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "AWS_SECRET_ACCESS_KEY"
+        value_source {
+          secret_key_ref {
+            secret  = google_secret_manager_secret.secret_access_key.secret_id
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "AWS_DEFAULT_REGION"
+        value_source {
+          secret_key_ref {
+            secret  = google_secret_manager_secret.default_region.secret_id
             version = "latest"
           }
         }
@@ -49,4 +96,3 @@ resource "google_cloud_run_v2_service" "env_variable_secret" {
   }
   depends_on = [google_secret_manager_secret_version.default]
 }
-
