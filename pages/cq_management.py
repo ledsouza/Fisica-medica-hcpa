8e2c05a83825bc935a6b5ba4c2a8e489000ad70f
Leandro Souza <leandro.souza.159@gmail.com>
2024-02-16 16:34:53 -0300

    Testes preliminares de conexao com o db

--- pages/cq_management.py
+++ pages/cq_management.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from PIL import Image
 from menu import menu_with_redirect
+import os
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import requests
+import pandas as pd
 
 st.set_page_config(page_title="Gerência de Controle de Qualidade", layout="wide")
 # Open an image file
@@ -9,3 +14,44 @@ st.sidebar.image(img, use_column_width=True)
 
 menu_with_redirect()
 
+uri = f"mongodb+srv://ledsouza:{os.getenv('MONGODB_PASSWORD')}@mnmanagement.opks2ne.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+    
+db = client['cq_gestao']
+collection = db['testes']
+
+lista_testes_periodicidade = {
+    'Uniformidade intrínseca para alta densidade de contagem': 'Mensal',
+    'Uniformidade para nuclídeos diferentes de Tc-99m': 'Anual',
+    'Uniformidade intrínseca com janelas energéticas assimétricas': 'Anual',
+    'Resolução e linearidade espacial intrínseca': 'Mensal',
+    'Resolução e linearidade espacial extrínseca': 'Mensal',
+    'Centro de rotação (SPECT)': 'Mensal',
+    'Resolução energética': 'Semestral'
+}
+
+teste = {
+    'Nome': 'Centro de Rotação',
+    'Identificação': 'GC 1',
+    'Data da Realização': '14/02/2024',
+    'Data da Próxima Realização': '14/03/2024',
+    'Arquivado': 'false'
+}
+
+#collection.insert_one(teste)
+
+# Convert the collection data to a DataFrame
+# equipamentos_diagnostico = pd.DataFrame(list(collection.find()))
+
+# st.dataframe(equipamentos_diagnostico)
+
+client.close()
